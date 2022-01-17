209931fab677d6e5e2ab01f7343ef323d02c8550
Kacper Wójtowicz <kacper.m.wojtowicz@gmail.com>
2022-01-17 19:43:44 +0100

    add basic validation

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show error
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerHTML = message;
+}
+
+// Show success
+function showSuccess(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+//  Event listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === '') {
+    showError(password2, 'Passwords are not the same');
+  } else {
+    showSuccess(password2);
+  }
+});
